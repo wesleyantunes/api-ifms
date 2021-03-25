3c8010fe6bfc9d38a3ddbea2179f44654d482aa2
Caio Maqueise Alécio Pinheiro <caio.maqueise@gmail.com>
2021-03-25 18:29:13 -0400

    ajustes

--- fake-api-filmes/app/js/app.js
+++ fake-api-filmes/app/js/app.js
@@ -4,28 +4,27 @@ async function getMovies(){
     const url = "http://localhost:3000/movies"
     const response = await axios.get(url)
     const movieList = Array.from(response.data)
-    movieList.forEach(function(movie){
+    movieList.forEach(async function(movie){
         apiData.innerHTML+=`
             <div class="card m-2" style="width:220px">
-                
-                <img src="${movie.poster}" class="card-img-top"/>   
-                
+               <img src="${movie.poster}">
                 <section class="card-body">
                     <h5 class="card-title">${movie.title}</h5>
-                    
                     <p>
                         Ano: ${movie.year}
                     </p>
                     <p>
-                    Genero: ${movie.genre}
+                        Genero: ${movie.genre}
                     </p>
                 </section>
             </div>
         `
-    })
-     
+    })   
 }
 getMovies()
+
+
+
   
 
 
