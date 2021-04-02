ff4c923dfced20abae76559ce372c09ff46e13e2
= <=>
2021-04-01 20:25:44 -0400

    api pokedex aula 01-04

---
+++ pokedex/app.js
@@ -0,0 +1,17 @@
+const pokeDex = document.querySelector('ul.pokedex')
+async function getPokemon(id){
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const urlImage = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`
+    const response = await axios.get(url)
+    const pokemon = response.data
+    const card = `
+        <li class="card">
+            <img src="${urlImage}" class="card-image">
+            <h1 class="card-title">${pokemon.name}</h1>
+        </li>
+    `
+    pokeDex.innerHTML+= card
+}
+
+getPokemon(1)
+getPokemon(15)
